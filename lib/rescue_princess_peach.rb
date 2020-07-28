65f881f4d076395a8069df601452c1fd7ffa5d77
Austin Cummings <aac729@live.com>
2020-07-28 16:16:33 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -3,7 +3,7 @@ def rescue_princess_peach
   puts "Hooray! Mario has rescued Princess Peach."
 end
 def catch_phrase
-  phrase = "Its-a me, Mario!"
+  phrase = "It's-a me, Mario!"
 end
 
 ## Comment the below back in to see method scope in action!
